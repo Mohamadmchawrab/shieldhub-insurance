e0b3228176b60f770778ca55d0224b56a0018c4e
Mo-mchawrab <mohamad@propertyvalue.ae>
2024-02-08 10:33:56 +0200

    add google analytics

--- pages/_document.js
+++ pages/_document.js
@@ -1,29 +1,43 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang="en">
       <Head>
         <link
-          rel='shortcut icon'
-          type='image/png'
-          sizes='32x32'
-          href='/logo.png'
+          rel="shortcut icon"
+          type="image/png"
+          sizes="32x32"
+          href="/logo.png"
         />
         <meta
-          name='keywords'
-          content='Insurance, Personal insurance, ShieldHub Insurance'
+          name="keywords"
+          content="Insurance, Personal insurance, ShieldHub Insurance"
         />
         <meta
-          name='description'
-          content='Protect your life and assets with ShieldHub Insurance. We offer a range of insurance products including auto, home, life, and health insurance. Our policies are designed to provide you with the coverage you need to safeguard your future. Contact us today for a personalized quote.'
+          name="description"
+          content="Protect your life and assets with ShieldHub Insurance. We offer a range of insurance products including auto, home, life, and health insurance. Our policies are designed to provide you with the coverage you need to safeguard your future. Contact us today for a personalized quote."
         />
-        <meta name='author' content='Darius Garcia Jr.'></meta>
+        <meta name="author" content="Darius Garcia Jr."></meta>
       </Head>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-RGSLM6HNWP"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'G-RGSLM6HNWP');
+              `,
+        }}
+      />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
