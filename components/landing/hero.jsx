c58057bdfacff399c0555c6be615977400b2ab48
Mo-mchawrab <mmchawrab@drimble.com>
2025-08-29 00:16:45 +0300

    Fixes

--- components/landing/hero.jsx
+++ components/landing/hero.jsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div className="relative w-full h-[55vh] md:h-[65vh] overflow-hidden">
+    <div className="relative w-full h-[60vh] md:h-[65vh] overflow-hidden">
       {/* Background Video */}
       <video
         className="absolute top-0 left-0 w-full h-full object-cover"
@@ -21,22 +21,19 @@ const Hero = () => {
       {/* Content */}
       <div className="relative z-10 flex flex-col md:flex-row items-center justify-evenly md:gap-20 px-4 text-white h-full">
         {/* Text */}
-        <div className="flex flex-col items-start  md:text-left">
-          <h1 className="text-2xl sm:text-3xl  md:text-5xl font-extrabold mb-3">
+        <div className="flex flex-col items-start text-center md:text-left">
+          <h1 className="text-xl sm:text-2xl md:text-4xl lg:text-5xl font-extrabold mb-2 sm:mb-3">
             Nabeel Simplifies Insurance
           </h1>
-          <p className="text-sm sm:text-base md:text-lg max-w-md text-gray-200 mb-2">
-            Protecting what matters most with innovative insurance solutions.
-          </p>
-          <p className="text-sm sm:text-base md:text-base max-w-md text-gray-200 mb-4">
+          <p className="text-xs sm:text-sm md:text-base max-w-sm sm:max-w-md text-gray-200 mb-3 sm:mb-4 text-left">
             Whether it’s your car, home, health, or business — Nabeel is here to
-            make insurance easy, accessible, and stress-free. Compare policies,
-            get instant quotes, and enjoy expert support every step of the way.
+            make insurance easy, and stress-free. Compare policies, get instant
+            quotes, and enjoy expert support every step of the way.
           </p>
 
           <Link
             href="#"
-            className="bg-green-800 hover:bg-green-700 text-white px-5 py-2.5 rounded-md font-semibold text-sm shadow-md"
+            className="bg-green-800 hover:bg-green-700 text-white px-4 sm:px-5 py-2 rounded-md font-medium text-xs sm:text-sm shadow-md"
             onClick={() => {
               const phoneNumber = "+96178791910";
               const message = encodeURIComponent(
@@ -50,13 +47,13 @@ const Hero = () => {
         </div>
 
         {/* Logo */}
-        <div className="md:pt-0">
+        <div className="mt-4 md:mt-0">
           <Image
             src="/nabeel-icons.png"
             width={230}
             height={80}
             alt="Logo"
-            className="w-36 sm:w-40 md:w-[260px] h-auto"
+            className="w-28 sm:w-36 md:w-[260px] h-auto"
           />
         </div>
       </div>
